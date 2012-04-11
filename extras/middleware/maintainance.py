565adbd28a6475069087535c4bb85828a62c708f
dushyant <dushyant@dubizzle.com>
2012-04-11 10:23:00 +0400

    adding a message on email beta list submission

--- extras/middleware/maintainance.py
+++ extras/middleware/maintainance.py
@@ -6,7 +6,9 @@ from extras.utils import save_email_in_file
 class MaintainanceMiddleware:
 
     def process_request(self, request):
+    	BETA_LIST = False
         if not (request.GET and 'pass' in request.GET and request.GET['pass'] == settings.MAINTAINANCE_SECRET):
             if request.POST:
                 save_email_in_file(request.POST)
-            return render_to_response('maintainance.html',{}, context_instance=RequestContext(request))
+                BETA_LIST = True
+            return render_to_response('maintainance.html',{'BETA_LIST':BETA_LIST,}, context_instance=RequestContext(request))
